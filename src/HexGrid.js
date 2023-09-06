641c043e9c316a2b9d80bba04ab624b62be8897b
famulus <famulus.fusion@gmail.com>
2023-09-06 15:29:30 -0400

    checkin

--- src/HexGrid.js
+++ src/HexGrid.js
@@ -1,21 +1,32 @@
-// src/HexGrid.js
-
 import React, { useEffect, useRef } from 'react';
 import { defineHex, Grid, rectangle } from 'honeycomb-grid'
+import { SVG } from '@svgdotjs/svg.js'
 
 const HexGrid = () => {
     const svgRef = useRef(null);
-
     useEffect(() => {
-const Tile = defineHex({ dimensions: 30 })
 
-const grid = new Grid(Tile, rectangle({ width: 10, height: 10 }))
 
-grid.forEach(console.log)
+
+        function renderSVG(hex: Hex) {
+            const draw = SVG().addTo('body').size('100%', '100%')
+            const polygon = draw
+            // create a polygon from a hex's corner points
+            .polygon(hex.corners.map(({ x, y }) => `${x},${y}`))
+            .fill('none')
+            .stroke({ width: 1, color: '#999' })
+
+            return draw.group().add(polygon)
+        }
+
+
+        // Define the hex with the origin set to 'topLeft' for rendering purposes
+        const Hex = defineHex({ dimensions: 30, origin: 'topLeft' })
+        const grid = new Grid(Hex, rectangle({ width: 1000, height: 1000 }))
 
         const svg = svgRef.current;
 
-        grid.forEach(hex => {});
+        grid.forEach(renderSVG);
     }, []);
 
     return <svg ref={svgRef} width="800" height="600" />;
