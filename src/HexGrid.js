42eab68b16f5ab8bbeb4ab746d71d0ced60674f4
famulus <famulus.fusion@gmail.com>
2023-09-08 14:41:37 -0400

    first blue hexes! mapping is wrong, and they persist past note off

--- src/HexGrid.js
+++ src/HexGrid.js
@@ -8,6 +8,20 @@ const HexGrid = (props) => {
     const svgRef = useRef(null);
     const gridRef = useRef(); // Use useRef to persist grid across re-renders
     const drawRef = useRef(); // Use useRef to persist draw across re-renders
+    function renderSVG(hex) {
+        // Create a polygon from a hex's corner points and add it to the existing SVG canvas
+        const hexPolygon = drawRef.current.polygon(hex.corners.map(({ x, y }) => `${x},${y}`))
+        .fill('none')
+        .stroke({ width: 1, color: '#555' })
+            .data('row', hex.row) // Add data-row attribute
+            .data('col', hex.col); // Add data-col attribute
+
+        // Add text to display the index
+            const center = hexToPoint(hex);
+            drawRef.current.text(`${hex.row},${hex.col}`)
+            .move(center.x, center.y)
+            .font({ anchor: 'middle', size: 12, fill: '#000' });
+    }
 
     useEffect(() => {
         console.log("useEffect is running");
@@ -15,20 +29,6 @@ const HexGrid = (props) => {
         // Create the SVG canvas once
         drawRef.current = SVG().addTo(svgRef.current).size('100%', '100%');
 
-        function renderSVG(hex) {
-            // Create a polygon from a hex's corner points and add it to the existing SVG canvas
-            const hexPolygon = drawRef.current.polygon(hex.corners.map(({ x, y }) => `${x},${y}`))
-            .fill('none')
-            .stroke({ width: 1, color: '#555' })
-                .data('row', hex.row) // Add data-row attribute
-                .data('col', hex.col); // Add data-col attribute
-
-            // Add text to display the index
-                const center = hexToPoint(hex);
-                drawRef.current.text(`${hex.row},${hex.col}`)
-                .move(center.x, center.y)
-                .font({ anchor: 'middle', size: 12, fill: '#000' });
-            }
 
         // Define the hex with the origin set to 'topLeft' for rendering purposes
             const Hex = defineHex({ dimensions: 30, origin: { x: -100, y: -100 } });
@@ -45,11 +45,11 @@ const HexGrid = (props) => {
 
 
             const hex = midiToHex(note);
-const hexPolygon = drawRef.current.find(`polygon[data-row="${hex.row}"][data-col="${hex.col}"]`).first();
+            const hexPolygon = drawRef.current.find(`polygon[data-row="${hex.row}"][data-col="${hex.col}"]`);
             console.log(`Hex [${hex.row}, ${hex.col}] -> Polygon:`, hexPolygon);
 
             if (hexPolygon) {
-                // hexPolygon.fill('blue');
+                hexPolygon.fill('blue');
             }
         });
 
