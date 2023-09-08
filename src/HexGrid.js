aafb40d76468466b6d6918b9324f45fd31bb031d
famulus <famulus.fusion@gmail.com>
2023-09-08 14:49:57 -0400

    checkin

--- src/HexGrid.js
+++ src/HexGrid.js
@@ -1,3 +1,4 @@
+// lates HexGrid.js
 import React, { useEffect, useRef } from 'react';
 import { defineHex, Grid, rectangle, hexToPoint } from 'honeycomb-grid';
 import { SVG } from '@svgdotjs/svg.js';
