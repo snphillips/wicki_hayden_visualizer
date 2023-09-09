ce91b5b4ccbf7c417b90039491685737231ed6dd
famulus <famulus.fusion@gmail.com>
2023-09-09 09:06:35 -0400

    whitespace

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
+import React, { useState } from "react"
+import MidiInstrument from "./midiInstrument"
 
-
-import React, { useState } from 'react';
-import MidiInstrument from './midiInstrument';
-
-import HexGrid from './HexGrid';
+import HexGrid from "./HexGrid"
 
 const App = () => {
-  const [activeNotes, setActiveNotes] = useState([]);
+  const [activeNotes, setActiveNotes] = useState([])
 
   const onMIDIMessage = (message) => {
-    let [status, note, velocity] = message;
-    console.log(`status:${status}, note: ${note },  velocity: ${velocity}`)
-
-    if ((status >= 0x90 && velocity === 0) || (status >= 0x80 && status < 0x90)) {
-
-      setActiveNotes(prevNotes => prevNotes.filter(n => n !== note));  
+    let [status, note, velocity] = message
+    // console.log(`status:${status}, note: ${note },  velocity: ${velocity}`)
+    if (status === 144 && velocity > 0) {
+      // note on
+      setActiveNotes((prevNotes) => [...new Set([...prevNotes, note])])
+    } else if (status === 128 || (status === 144 && velocity === 0)) {
+      // Handle note off
+      setActiveNotes((prevNotes) => prevNotes.filter((n) => n !== note))
     }
-    else {
-    // setActiveNotes(prevNotes => prevNotes.filter(n => n !== note));
-      setActiveNotes(prevNotes => [... new Set( [...prevNotes, note] )]);  
-    // setActiveNotes(prevNotes => [...prevNotes, note]);  
-      
-    }
-
-  };
+  }
 
   return (
     <div>
-    <MidiInstrument onMIDIMessage={onMIDIMessage} />
-    <HexGrid activeNotes={activeNotes} />
+      <MidiInstrument onMIDIMessage={onMIDIMessage} />
+      <HexGrid activeNotes={activeNotes} />
     </div>
-    );
-};
+  )
+}
 
-export default App;
+export default App
