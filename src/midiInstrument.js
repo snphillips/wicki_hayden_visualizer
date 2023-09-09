36b45d4139f8950b6cc91b166065a67ac6646f1b
famulus <famulus.fusion@gmail.com>
2023-09-09 09:07:31 -0400

    formatting

--- src/midiInstrument.js
+++ src/midiInstrument.js
@@ -1,38 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react"
 
 const MidiInstrument = ({ onMIDIMessage }) => {
-    const [midiSupported, setMidiSupported] = useState(false);
-    const [midiMessages, setMidiMessages] = useState([]);
+    const [midiSupported, setMidiSupported] = useState(false)
+    const [midiMessages, setMidiMessages] = useState([])
 
     useEffect(() => {
         if (navigator.requestMIDIAccess) {
-            setMidiSupported(true);
-            navigator.requestMIDIAccess({ sysex: true })
-                .then(onMIDISuccess, onMIDIFailure);
+            setMidiSupported(true)
+            navigator
+                .requestMIDIAccess({ sysex: true })
+                .then(onMIDISuccess, onMIDIFailure)
         } else {
-            console.log('WebMIDI is not supported in this browser.');
+            console.log("WebMIDI is not supported in this browser.")
         }
 
         function onMIDISuccess(midiAccess) {
-            const inputs = midiAccess.inputs.values();
-            for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
-                input.value.onmidimessage = handleMIDIMessage;
+            const inputs = midiAccess.inputs.values()
+            for (
+                let input = inputs.next();
+                input && !input.done;
+                input = inputs.next()
+            ) {
+                input.value.onmidimessage = handleMIDIMessage
             }
         }
 
         function onMIDIFailure(error) {
-            console.error('Could not access your MIDI devices.', error);
+            console.error("Could not access your MIDI devices.", error)
         }
 
         function handleMIDIMessage(message) {
-            let [status, note, velocity] = message.data;
-                if (note) {
-                onMIDIMessage(message.data);
+            let [status, note, velocity] = message.data
+            if (note) {
+                onMIDIMessage(message.data)
                 // console.log(`status: ${status}, note: ${note },  velocity: ${velocity}`)
-
-                }
+            }
         }
-    }, [onMIDIMessage]);
+    }, [onMIDIMessage])
 
     return (
         <div>
@@ -45,7 +49,7 @@ const MidiInstrument = ({ onMIDIMessage }) => {
                 <p>MIDI is not supported in this browser.</p>
             )}
         </div>
-    );
-};
+    )
+}
 
-export default MidiInstrument;
+export default MidiInstrument
