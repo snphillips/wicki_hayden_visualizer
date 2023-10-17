4477af5f1bb8a967cef1f56d4e2ce3e59aaa4b59
Sarah Phillips <snphillips@gmail.com>
2023-10-17 08:53:03 -0400

    style instructions

--- src/Instructions.tsx
+++ src/Instructions.tsx
@@ -2,7 +2,7 @@ const Instructions = () => {
   return (
     <div id="instructions">
       <h1>Wicki Hayden Visualizer</h1>
-      <p>This app visualizes MIDI signals onto a Wicki Hayden note layout. Use this app to:</p>
+      <p>This app visualizes MIDI signals onto a Wicki Hayden note layout.</p>
       <p>Use this app to:</p>
       <p>Connect and play your favorite MIDI controller. Watch as corresponding notes light up. </p>
       <p>Stream a MIDI file from a digital audio workstation (DAW), like Ableton.</p>
